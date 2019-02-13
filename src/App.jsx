7ddc5343eb5a814e4d70340225184136a512f9a9
Lwong01 <leon.lo.wong@gmail.com>
2019-02-13 01:49:59 +0000

    adding ChatBar and Message as Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    // this is the *only* time you should assign directly to state:
+    this.state = {
+      currentUser: {
+        name: "Bob"
+      },
+      messages: [
+        {
+          id: "1",
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: "2",
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <div>
+          <MessageList messages={this.state.messages} />
+        </div>
+        <div>
+          <ChatBar username={this.state.currentUser.name} />
+        </div>
+      </div>
+
     );
   }
 }
-export default App;
+// export default App;
