73ad13844117cddcded46d45d89f8a9f852d1b23
Lwong01 <leon.lo.wong@gmail.com>
2019-02-13 03:51:15 +0000

    Rohit logic

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,27 @@ export default class App extends Component {
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
         }
       ]
-    };
+    }
+    this.addMessage = this.addMessage.bind(this);
+  }
+
+  addMessage(newMessage) {
+    console.log("we are in the App component");
+    console.log(newMessage);
+    //this.setState({ messages: messages })
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({ messages: messages })
+    }, 3000);
   }
 
   render() {
@@ -32,7 +52,7 @@ export default class App extends Component {
           <MessageList messages={this.state.messages} />
         </div>
         <div>
-          <ChatBar username={this.state.currentUser.name} />
+          <ChatBar username={this.state.currentUser.name} addMessage = {this.addMessage} />
         </div>
       </div>
 
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,13 +4,21 @@ import React, { Component } from 'react';
 export default class ChatBar extends Component {
   constructor(props) {
     super(props);
+
+    this.createNewMessage = this.createNewMessage.bind(this);
+  }
+  createNewMessage(event){
+    if(event.key==="Enter"){
+      console.log("we are pressed enter in the chatbar")
+      this.props.addMessage("hello");
+    }
   }
 
   render() {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" placeholder="Your Name (Optional)" value={this.props.username} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress = {this.createNewMessage} />
       </footer>
     )
   }
